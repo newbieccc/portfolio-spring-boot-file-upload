72d2bc312ac9670da6ecb692e3df51fbe7c15f66
sjseo90 <sjseo90@gmail.com>
2026-03-06 13:23:48 +0900

    chore: 프로젝트 부팅 및 Swagger/H2 기본 실행 확인

---
+++ src/test/java/com/portfolio/fileupload/PortfolioSpringBootFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSpringBootFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
